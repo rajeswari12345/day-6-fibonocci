c293ad69ae0b519e2fc959e1c671d17b96aa4976
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-04 15:40:48 +0530

    day 6 perfect num

---
+++ day 6 perfect num.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more informatio
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, sum = 0, n;
+            Console.Write("enter the Number");
+            number = int.Parse(Console.ReadLine());
+            n = number;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == n)
+            {
+                Console.WriteLine("\n Entered number is a perfect number");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("\n Entered number is not a perfect number");
+                Console.ReadLine();
+            }
+        }
+    }
+}
