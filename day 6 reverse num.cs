5c8c7941aa3d27992d16b540e3cd4a6088ef1c8a
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-04 15:32:03 +0530

    day 6 reverse num

---
+++ day 6 reverse num.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using System;  
+  public class ReverseExample
+{
+    public static void Main(string[] args)
+    {
+        int n, reverse = 0, rem;
+        Console.Write("Enter a number: ");
+        n = int.Parse(Console.ReadLine());
+        while (n != 0)
+        {
+            rem = n % 10;
+            reverse = reverse * 10 + rem;
+            n /= 10;
+        }
+        Console.Write("Reversed Number: " + reverse);
+    }
+}
